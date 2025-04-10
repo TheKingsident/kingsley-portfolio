4118079249a388e1ec943a8ef56c445f54a12264
TheKingsident <kingsident@gmail.com>
2025-04-10 12:33:14 +0100

    Refactor Home component: adjust section height and update image background for improved layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <section id="home" className="relative flex flex-col md:flex-row items-center justify-between w-full min-h-screen px-8 md:px-16 bg-gradient-to-br from-white to-[#f7faff] overflow-hidden">
+    <section id="home" className="relative flex flex-col md:flex-row items-center justify-between w-full h-screen px-8 md:px-16 bg-gradient-to-br from-white to-[#f7faff] overflow-hidden">
       {/* Floating Circles */}
       <div className="absolute w-32 h-32 bg-orange-300 rounded-full opacity-50 animate-float top-10 left-10"></div>
       <div className="absolute w-24 h-24 bg-blue-300 rounded-full opacity-50 animate-float-delayed bottom-20 right-20"></div>
@@ -36,8 +36,8 @@ const Home = () => {
       </div>
 
       {/* Image Section */}
-      <div className="relative w-[300px] md:w-[450px] lg:w-[500px] mt-12 md:mt-0">
-        <div className="absolute w-[600px] h-[600px] bg-[#ffe0cc] -z-10"></div>
+      <div className="relative w-[300px] md:w-[450px] lg:w-[500px] max-h-[50vh] mt-12 md:mt-0">
+        <div className="absolute w-[400px] h-[400px] bg-[#ffe0cc] rounded-full animate-float-delayed"></div>
         <img
           src="src/assets/profile-main2.png"
           alt="Profile"
