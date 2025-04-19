48b516aefcb27e77305425f3e6a446d9e4b82c03
TheKingsident <kingsident@gmail.com>
2025-04-19 12:26:04 +0100

    Enhance ItemDetailsPopUp with fade-in animation and update Portfolio to manage selected item state

--- src/components/ItemDetailsPopUp.tsx
+++ src/components/ItemDetailsPopUp.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const ItemDetailsPopUp: React.FC<Props> = ({ item, onClose }) => {
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-            <div className="bg-white rounded-lg shadow-lg p-6 w-96">
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50 animate-fade-in">
+            <div className="bg-white rounded-lg shadow-lg p-6 w-96 transition-opacity duration-300">
                 <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-800">
                     &times;
                 </button>
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCube } from '@fortawesome/free-solid-svg-icons';
 import { PortfolioItem } from '../data/portfolioItemCardData';
 import portfolioItemCardData from '../data/portfolioItemCardData';
 import PortfolioItemCard from '../components/PortfolioItem';
+import ItemDetailsPopUp from '../components/ItemDetailsPopUp';
+
 
 const Portfolio = () => {
+
+  const [selectedItem, setSelectedItem] = useState<PortfolioItem | null>(null);
+
+  useEffect(() => {
+    if (selectedItem) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [selectedItem]);
+
   return (
     <section className="min-h-screen bg-gradient-to-r from-blue-50 to-rose-50 flex flex-col px-8 md:px-16 py-12 border-r-1 border-amber-50 overflow-hidden">
       <div className="flex items-center gap-2 bg-white px-10 py-1 rounded-full w-fit text-sm">
@@ -17,12 +34,22 @@ const Portfolio = () => {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           {portfolioItemCardData.map((item: PortfolioItem, index: number) => (
-            <PortfolioItemCard key={index} item={item} />
+            <PortfolioItemCard
+              key={index}
+              item={item}
+              onClick={setSelectedItem} />
           ))}
 
         </div>
 
       </div>
+
+      {selectedItem && (
+        <ItemDetailsPopUp
+          item={selectedItem}
+          onClose={() => setSelectedItem(null)}
+        />
+      )}
       
       
     </section>
