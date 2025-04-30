774ad6ce2080c4426827489906ee3471204227ef
TheKingsident <kingsident@gmail.com>
2025-04-30 21:11:53 +0100

    Implement email notifications for contact form submissions

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+
+dotenv.config({ path: "../.env" });
 
 export const handleFormSubmission = async (req, res) => {
   const { captchaToken, formData } = req.body;
 
-  // Verify reCAPTCHA token
   try {
     const response = await axios.post(
       `https://www.google.com/recaptcha/api/siteverify`,
@@ -20,12 +23,33 @@ export const handleFormSubmission = async (req, res) => {
       return res.status(400).json({ error: "reCAPTCHA verification failed" });
     }
 
-    // Process the form data (e.g., save to database, send email, etc.)
-    console.log("Form Data:", formData);
+    const transporter = nodemailer.createTransport({
+      host: "smtp.zoho.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.SITE_EMAIL,
+        pass: process.env.SITE_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: `${formData.name} on Kingsley Usa's Portfolio <${process.env.SITE_EMAIL}>`,
+      to: process.env.SITE_EMAIL,
+      subject: `New Contact Form Submission: ${formData.subject}`,
+      text: `
+        Name: ${formData.name}
+        Email: ${formData.email}
+        Subject: ${formData.subject}
+        Message: ${formData.message}
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
 
     res.status(200).json({ message: "Form submitted successfully" });
   } catch (error) {
-    console.error("Error verifying reCAPTCHA:", error);
+    console.error("Error processing form submission:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
