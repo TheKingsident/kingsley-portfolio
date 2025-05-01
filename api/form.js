ec3e0950774ea30de49188dbb0bf5af62a180be5
TheKingsident <kingsident@gmail.com>
2025-05-01 15:18:38 +0100

    Fix error message in form handler and update API fetch path in Contact component

--- api/form.js
+++ api/form.js
@@ -6,7 +6,7 @@ dotenv.config({ path: "../.env" });
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
+    return res.status(405).json({ error: 'Methods Not Allowed' });
   }
 
   const { captchaToken, formData } = req.body;
