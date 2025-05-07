395ce562e7cb522979a9529a97d447fcf166468d
TheKingsident <kingsident@gmail.com>
2025-05-07 12:03:16 +0100

    feat: update release configuration to include production branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    branches: ["main"],
+export default {
+    branches: ["main", "production"],
     plugins: [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
